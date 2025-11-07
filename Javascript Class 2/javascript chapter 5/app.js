d88ab4d68a27964a02df1f7bf60cb7c4a187d711
sobanahmed099 <sobanahmedkigs125@gmail.com>
2025-11-07 23:20:20 +0500

    Add files via upload

---
+++ Javascript Class 2/javascript chapter 5/app.js
@@ -0,0 +1,70 @@
+document.write(" <h3 >sum of 5 and 3 is 8 </h3>");
+document.write(" <h3> Subtraction of 5 and 3 is 2 </h3>");
+document.write(" <h3> Division of 10 by 2 = 5 </h3>");
+document.write(" <h3> Modulus of 15 and 5 = 0 </h3> <br> <br>"  );
+
+
+
+var x;
+document.write(" <h3>Value after variable declaration is undefined </h3> ");
+x = (" 15");
+document.write(" Initialized value :   "+ x +"  <br>")
+document.write(" Value after increment is " + ++ x  + " <br>")
+ 
+var x = (16+7)
+document.write(" Value after adding 7 is = "+ x +" <br> ");
+document.write(" Value after decrement is " +  --x  + " <br>")
+var x = (22 % 3)
+document.write(" The remainder is "+ x +"  <br><br><br>")
+
+
+
+let ticketprice = ("600")
+let numberOftickets =(" 5")
+let totalcost = (  ticketprice * numberOftickets )
+document.write(" <h3><b>Total cost to buy 5 tickets to a movie is "+ totalcost +" PKR </b></h3> <br><br>")
+
+
+document.write(" <h3>Table of 4 </h3> 4x1=4<br> 4x2=8<br> 4x3=12<br> 4x4=16<br>  4x5=20<br> 4x6=24<br> 4x7=28<br> 4x8=32<br> 4x9=36<br> 4x10=40<br>")
+var grandtotal = ( 650*3 + 100*7 + 100);
+
+document.write(" <h1> <b> Shopping Cart </b></h1> <br><br><br> Price of item1 is 650 <br> Quantity of item 1 is 3 <br> Price of item 2 is 100 <br> Quantity of item 2 is 7 <br> Shipping Charges 100 <br> <br> Total cost of your goods is "+ grandtotal +" <br>  ")
+
+
+var per = ( 804/980*100)
+document.write("<h1><b> Mark Sheet </b></h1> <br><br><br> Total marks: 980 <br> Marks Obtained: 804 <br> Percebtage: "+ per +"     ")
+
+var Currency = (10*104.80 + 25*28)
+document.write("<h1><b>Currency in PKR   </h1></b> <br><br> Total Currency in PKR: "+ Currency +" <br> <br>  ") 
+
+var b = (3);
+var c = ( 3+5*10/2)
+document.write(" <b>Problem No:10 </b> <br> "+ c +" <br> ")
+
+
+var currentyear =(2025)
+var birthyear =( 1937)
+var age =( currentyear-birthyear)
+
+document.write("<h1><b>Age Calculator </h1></b><br> Current Year :2025 <br> Birth Year: 1937 <br> Your Age: "+ age+"  ")
+
+var radi= 20
+var curcumference = (2*3.14*20)
+var area= ( 3.14*20*20)
+document.write("<h1><b> The Geometrizer </h1></b> <br> <br> Radius of a circle is: "+ radi+"<br>The curcumference of that circle is: "+ curcumference+"<br> The area is: "+ area+"<br><br> ")
+
+
+
+
+var snack = ("Chocolate Chips")
+var currentage =(20);
+var estimatedage = (65);
+var remainingtime = (45);
+var amount =(5)
+
+var grandtotal =( remainingtime *5 )
+document.write("<h1><b> The Lifetime Supply Calculator </h1></b>  <br> <br> Favourite Snack:Chocolate Chips <br>  Current Age: "+ currentage+" <br> Estimated Maximum Age; "+ estimatedage +" <br> Amount Of Snack Per Day: "+ amount+" <br> You will need "+ grandtotal +" Chocolate Chips to last you until the rip old age of 65 ")
+
+
+
+
