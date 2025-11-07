85aee8ca7a9ed00ea9ba35b6253934481f57e2c3
sobanahmed099 <sobanahmedkigs125@gmail.com>
2025-11-07 23:29:15 +0500

    Update app.js

--- Javascript Class 2/Javascript chapter 4/app.js
+++ Javascript Class 2/Javascript chapter 4/app.js
@@ -17,4 +17,23 @@
 
 
 document.write(" <h1> <b> Rules for naming JS variables </b> </h1>");
-document.write(Variable names can only contain )
+
+let letters = prompt(" Enter what variable name can contain beside numbers ,  ");
+let dollar = prompt("Enter symbol variable  can contain ,");
+let underscore = prompt("Enter another symbol  variable can contain beside numbers , ");
+
+document.write(" Variable names can only contain " + letters + "1 , " + dollar + " or " + underscore + ". <br> For Exapmle: name1, _name , or $name <br><br>"); 
+
+let dollarsign = prompt(" Enter what comes before a name,  ");
+let underscoresign = prompt(" Enter what other comes before a name , ");
+let letter = prompt("Enter what variable name can come without symbol , ");
+
+
+document.write("Variables must begin with a " + dollarsign + ",  " + underscoresign + " or  " + letter + " <br> For Example: $name,_name or name . <br><br>")
+
+
+let enter = "case sensitive";
+document.write(" Variables names must be " + enter + " <br><br> ")
+
+let keyword = " keyword"
+document.write(" Variables names should not ne JS " + keyword+ " ")
